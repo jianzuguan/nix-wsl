803d4ce6f9612308604f420127b1bc7d99cc84c1
Zo <git@jzg.fastmail.com>
2025-02-09 22:39:07 +0000

    Disable unused starship modules and update prompt format in home.nix

--- home.nix
+++ home.nix
@@ -106,17 +106,21 @@ in {
     # FIXME: disable this if you don't want to use the starship prompt
     starship.enable = true;
     starship.settings = {
-      aws.disabled = true;
-      gcloud.disabled = true;
-      kubernetes.disabled = false;
       git_branch.style = "242";
       directory.style = "blue";
       directory.truncate_to_repo = false;
       directory.truncation_length = 8;
+      aws.disabled = true;
+      gcloud.disabled = true;
+      kubernetes.disabled = true;
       python.disabled = true;
       ruby.disabled = true;
       hostname.ssh_only = false;
-      hostname.style = "bold green";
+      # hostname.style = "bold green";
+      format = ''
+        $cmd_duration
+        $directory$git_branch
+        $character'';
     };
 
     # FIXME: disable whatever you don't want
