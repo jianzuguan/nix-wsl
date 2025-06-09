1df7e352be8e4956e0859d366c82541a6fd670db
Zo <git@jzg.fastmail.com>
2025-06-09 23:45:29 +0000

    Add server and host configuration files for NixOS setup

--- flake.nix
+++ flake.nix
@@ -82,7 +82,7 @@
         username = "nixos";
         modules = [
           nixos-wsl.nixosModules.wsl
-          ./nix/wsl.nix
+          ./hosts/nixos/configurations.nix
         ];
       };
     };
