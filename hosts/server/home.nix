3958932f596ca9d05d906d564ac20944d2db5ee7
Zo <git@jzg.fastmail.com>
2025-06-10 19:38:34 +0000

    Refactor home.nix: streamline package list and improve organization

--- hosts/server/home.nix
+++ hosts/server/home.nix
@@ -1,57 +1,25 @@
 {
-  # FIXME: uncomment the next line if you want to reference your GitHub/GitLab access tokens and other secrets
-  # secrets,
-  pkgs,
-  username,
-  nix-index-database,
-  ...
-}: let
+# FIXME: uncomment the next line if you want to reference your GitHub/GitLab access tokens and other secrets
+# secrets,
+pkgs, username, nix-index-database, ... }:
+let
   stable-packages = with pkgs; [
     bottom
+    corepack
     coreutils
     curl
-    # du-dust
-    # fd
-    # findutils
-    # fx
+    fnm
+    fzf
+    gh
     git
-    git-crypt
-    htop
+    jdk
     jq
-    # killall
-    # mosh
-    # procs
-    # ripgrep
-    # sd
+    lsd
     tree
     unzip
     vim
     wget
     zip
-    # key tools
-    gh # for bootstrapping
-    # just
-
-    # core languages
-    # rustup
-
-    # rust stuff
-    # cargo-cache
-    # cargo-expand
-
-    # local dev stuf
-    # mkcert
-    # httpie
-
-    fnm
-    corepack
-    jdk
-    go
-
-    # formatters and linters
-    alejandra # nix formatter
-    statix # Lints and suggestions for the Nix programming language
-
     zoxide
   ];
 in {
@@ -63,26 +31,24 @@ in {
     ../../modules/starship.nix
   ];
 
-  home.stateVersion = "22.11";
-
   home = {
+    stateVersion = "22.11";
+
     username = "${username}";
     homeDirectory = "/home/${username}";
 
     sessionVariables.EDITOR = "nvim";
-    # FIXME: set your preferred $SHELL
     sessionVariables.SHELL = "/etc/profiles/per-user/${username}/bin/fish";
+
+    packages = stable-packages;
   };
-
-  home.packages = stable-packages ++ unstable-packages;
-
   programs = {
     home-manager.enable = true;
+
     nix-index.enable = true;
     nix-index.enableFishIntegration = true;
     nix-index-database.comma.enable = true;
 
-    # FIXME: disable whatever you don't want
     fzf.enable = true;
     fzf.enableFishIntegration = true;
     lsd.enable = true;
@@ -90,18 +56,10 @@ in {
     lsd.settings = {
       #https://github.com/lsd-rs/lsd#config-file-content
       date = "+%Y-%m-%d %H:%M:%S";
-      icons = {
-        when = "never";
-      };
+      icons = { when = "never"; };
       sorting.dir-grouping = "first";
     };
     zoxide.enable = true;
     zoxide.enableFishIntegration = true;
-    broot.enable = true;
-    broot.enableFishIntegration = true;
-    direnv.enable = true;
-    direnv.nix-direnv.enable = true;
-
-    
   };
 }
